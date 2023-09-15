b84a0dd3ab186d638bcddc7a902d97894a4f2212
Jessica Dea <jessicardea@gmail.com>
2023-09-14 23:36:11 -0700

    Reorganized code, added random color to circle

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
 import React from 'react';
 import './App.css';
 
-function ChooseColor() {
-  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-}
 
 function Circles() {
-  const circleStyle = {
-    height: "25px",
-    width: "25px",
-    backgroundColor: "blue",
-    borderRadius: "50%",
-    display: "inline-block",
-  }
+  const [numCircles, setNumCircles] = React.useState(4);
+
+  const randomColor = () => {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  };
+
   return (
     <div>
-      <button id="circle" style={circleStyle}></button>
+      <button id="circle" style={{ backgroundColor: randomColor() }}></button>
     </div >
   )
 }
 
 
 function App() {
-  const [numCircles, setNumCircles] = React.useState(4)
-  const circles = Array.from({ length: numCircles }, (_, index) => (
-    <Circles key={index} />
-  ));
-
   return (
     <div className="App">
-      {circles}
+      <Circles />
     </div>
   );
 }
