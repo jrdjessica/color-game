ae94fcc2bdc54a5ec0666cb64353a7fd7bbff11a
Jessica Dea <jessicardea@gmail.com>
2023-09-23 19:34:09 -0700

    Created resetColors with game logic

--- src/App.js
+++ src/App.js
@@ -4,21 +4,26 @@ import './App.css';
 
 function Circles() {
   const [numCircles, setNumCircles] = React.useState(4);
-  const [difIndexCircle, setDifIndexCircle] = React.useState(Math.floor(Math.random() * numCircles))
+  const [difIndexCircle, setDifIndexCircle] = React.useState(null)
   const [randomColor, setRandomColor] = React.useState('black');
 
-  const pickRandomColor = () => {
-    setRandomColor('#' + Math.floor(Math.random() * 16777215).toString(16));
-  };
-
   const circleElements = [];
+
   for (let i = 0; i < numCircles; i++) {
+    const circleColor = i === difIndexCircle ? 'red' : randomColor
+
+    const resetColors = () => {
+      setRandomColor('#' + Math.floor(Math.random() * 16777215).toString(16));
+      setDifIndexCircle(Math.floor(Math.random() * numCircles));
+    }
+
     circleElements.push(
       <button
         key={i}
         id="circle"
         className="circle"
-        style={{ backgroundColor: randomColor }}
+        style={{ backgroundColor: circleColor }}
+        onClick={resetColors}
       ></button>
     );
   }
