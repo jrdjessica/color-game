30ba51acfe6b3b1d7cc3fe2a462731053ff77410
Jessica Dea <jessicardea@gmail.com>
2023-09-24 23:23:16 -0700

    Fixed re-render logic

--- src/App.js
+++ src/App.js
@@ -3,39 +3,40 @@ import './App.css';
 
 
 function Circles() {
-  const [numCircles, setNumCircles] = React.useState(4);
-  const [difIndexCircle, setDifIndexCircle] = React.useState('')
+  const [numCircles, setNumCircles] = React.useState(8);
+  const [difIndexCircle, setDifIndexCircle] = React.useState(null)
   const [randomColor, setRandomColor] = React.useState('black');
   const [score, setScore] = React.useState(0)
 
   const circleElements = [];
 
+  const resetLevel = (i) => {
+    if (difIndexCircle === i) {
+      setScore(score + 1)
+    }
+
+    setRandomColor('#' + Math.floor(Math.random() * 16777215).toString(16));
+    setDifIndexCircle(Math.floor(Math.random() * numCircles));
+  }
+
   for (let i = 0; i < numCircles; i++) {
     const circleColor = i === difIndexCircle ? 'red' : randomColor
-
-    const resetLevel = () => {
-      if (difIndexCircle === i) {
-        setScore(score + 1)
-      }
-
-      setRandomColor('#' + Math.floor(Math.random() * 16777215).toString(16));
-      setDifIndexCircle(Math.floor(Math.random() * numCircles));
-      console.log(i)
-    }
 
     circleElements.push(
       <button
         key={i}
-        id="circle"
+        id={i}
         className="circle"
         style={{ backgroundColor: circleColor }}
-        onClick={resetLevel}
+        onClick={() => resetLevel(i)}
       ></button>
     );
   }
 
+  const containerKey = `container-${difIndexCircle}`;
+
   return (
-    <div>
+    <div key={containerKey}>
       Score: {score}
       <div className="circle-container">{circleElements}</div>
     </div>
